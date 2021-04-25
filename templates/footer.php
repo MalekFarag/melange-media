89314471d1984dad739374b43f21e17da51e3e00
Malek Farag <faragmalek14@gmail.com>
2021-04-25 17:04:56 -0400

    removed contact

--- templates/footer.php
+++ templates/footer.php
@@ -19,7 +19,7 @@
                 
 
                 <li><a href="./about">About Us</a></li>
-                <li><a href="./contact">Contact Us</a></li>
+                <!-- <li><a href="./contact">Contact Us</a></li> -->
             </ul>
         </nav>
 
